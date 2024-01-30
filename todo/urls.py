ff58db206617c298b6daccf8429a3051d713230c
Azzlem <azzlem.sid@gmail.com>
2024-01-30 22:03:56 +0300

    добавил возможность создавать и изменять дела

--- todo/urls.py
+++ todo/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 
 from todo import views
 from todo.apps import TodoConfig
-from todo.views import TodoListView
+from todo.views import TodoListView, TodoCreateView, TodoUpdateView
 
 app_name = TodoConfig.name
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('todo_list/', TodoListView.as_view(), name='todo_list'),
-
+    path('todo_form/', TodoCreateView.as_view(), name='todo_form'),
+    path('todo_update/<int:pk>/', TodoUpdateView.as_view(), name='todo_update'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,33 @@
+from django.urls import reverse
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
 from django.shortcuts import render
 
+from todo.forms import TodoForm
 from todo.models import Todo
 
 
 def index(request):
-    return render(request, "base.html")
+    return render(request, "todo/base.html")
 
 
 class TodoListView(ListView):
     model = Todo
-    template_name = "todo_list.html"
+    template_name = "todo/todo_list.html"
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    form_class = TodoForm
+    template_name = "todo/todo_form.html"
+
+    def get_success_url(self):
+        return reverse('todo:todo_list')
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    form_class = TodoForm
+
+    def get_success_url(self):
+        return reverse('todo:todo_list')
